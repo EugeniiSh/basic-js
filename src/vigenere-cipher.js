789109764d5e1e85c82f88692c7e33dee7e764b3
EugeniiSh <evgenii.shmerko@mail.ru>
2023-12-22 15:51:27 +0300

    feat: add last tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,102 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) 
+  {
+      this.direction = direction === false? false:true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(message === undefined || key === undefined)
+    {
+      throw TypeError('Incorrect arguments!')
+    }
+
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";		
+        
+    let result = '';
+    let ratio = 0; //корректировочный коэффициент при отсутствии символа в алфавите.
+    let i = 0; //Дополнительный счётчик
+    // const maxLength = Math.max(message.length, key.length);
+    const maxLength = message.length;
+
+    for(let w = 0; w < maxLength; w++)
+    {
+      i = w - ratio;
+
+      // Подгогнка ключа\сообщения под maxLegth и получение текущего символа
+      const currentMessageWord = message[((w >= message.length) ? w % message.length : w )].toUpperCase();
+      const currentKeyWord = key[((i >= key.length) ? i % key.length : i )].toUpperCase();
+      //Обработка не алфавитных символов
+      if(abc.indexOf(currentMessageWord) === -1)
+      {
+        result += currentMessageWord;
+        ratio++;
+        continue;
+      }
+      
+      //Получение индекса буквы.
+      const m_Index = abc.indexOf(currentMessageWord)
+      const k_Index = abc.indexOf(currentKeyWord)
+      //Получение зашифрованной буквы.
+      const c_Word = abc[((abc.length + (m_Index + k_Index)) % abc.length)];
+      
+      result += c_Word;
+    }
+    
+    if(this.direction === false)
+    {
+      result = result.split('').reverse().join(''); 
+    }
+    
+    return result;
+  }
+  decrypt(message, key) {
+    if(message === undefined || key === undefined)
+    {
+      throw TypeError('Incorrect arguments!')
+    }
+
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";		
+        
+    let result = '';
+    let ratio = 0; //корректировочный коэффициент при отсутствии символа в алфавите.
+    let i = 0; //Дополнительный счётчик
+    // const maxLength = Math.max(message.length, key.length);
+    const maxLength = message.length;
+
+    for(let w = 0; w < maxLength; w++)
+    {
+      i = w - ratio;
+
+      // Подгогнка ключа\сообщения под maxLegth и получение текущего символа
+      const currentMessageWord = message[((w >= message.length) ? w % message.length : w )].toUpperCase();
+      const currentKeyWord = key[((i >= key.length) ? i % key.length : i )].toUpperCase();
+      //Обработка не алфавитных символов
+      if(abc.indexOf(currentMessageWord) === -1)
+      {
+        result += currentMessageWord;
+        ratio++;
+        continue;
+      }
+      
+      //Получение индекса буквы.
+      const m_Index = abc.indexOf(currentMessageWord)
+      const k_Index = -abc.indexOf(currentKeyWord)
+      //Получение зашифрованной буквы.
+      const c_Word = abc[((abc.length + (m_Index + k_Index)) % abc.length)];
+      
+      result += c_Word;
+    }
+    
+    if(this.direction === false)
+    {
+      result = result.split('').reverse().join(''); 
+    }
+    
+    return result;
   }
 }
-
+// https://www.youtube.com/watch?v=KST4bGAH-8Y
 module.exports = {
   VigenereCipheringMachine
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(date === undefined)
+  {
+    return 'Unable to determine the time of year!';
+  }
+
+  if ((date instanceof Date) === false || Object.getOwnPropertyNames(date).length != 0 ) 
+  {
+    throw Error("Invalid date!");
+  }
+
+  let seasonNum;
+  try
+  {
+    seasonNum = date.getMonth();
+  }
+  catch(error)
+  {
+    throw TypeError('Invalid date!')
+  }
+    
+  switch(true)
+  {
+    case((seasonNum >= 0 && seasonNum <= 1) || seasonNum === 11):
+        return 'winter';
+    case(seasonNum >= 2 && seasonNum <= 4):
+        return 'spring';
+    case(seasonNum >= 5 && seasonNum <= 7):
+        return 'summer';
+    case(seasonNum >= 8 && seasonNum <= 10):
+        return 'autumn';    
+  }
 }
 
 module.exports = {
